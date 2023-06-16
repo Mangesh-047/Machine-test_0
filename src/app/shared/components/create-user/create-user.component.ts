ffed68f2704629fd9c107c7c67a752fd1d838121
Mangesh047 <dholem39@gmail.com>
2023-06-16 11:45:08 +0530

    form eye

--- src/app/shared/components/create-user/create-user.component.ts
+++ src/app/shared/components/create-user/create-user.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateUserComponent implements OnInit {
 
+
+  inputType: string = 'password'
+  inputType2: string = 'password'
+
+  icon: string = 'remove_red_eye'
+  icon2: string = 'remove_red_eye'
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeInputType() {
+
+    this.inputType === 'password' ? this.inputType = 'text' : this.inputType = 'password'
+    this.icon2 === 'remove_red_eye' ? this.icon2 = 'remove_eye' : this.icon2 = 'remove_red_eye'
+
+
+  }
+
+
+  changeInputType2() {
+
+    this.inputType2 === 'password' ? this.inputType2 = 'text' : this.inputType2 = 'password'
+
+    this.icon === 'remove_red_eye' ? this.icon = 'remove_eye' : this.icon = 'remove_red_eye'
+
   }
 
 }
